cf6e635e36cdd276f85689c6c277045977173c4f
XlentAcademy <xlentclass@gmail.com>
2021-03-23 22:42:40 +0530

    resolved bugs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, Response
 from gtts import gTTS
+import os
 
 app = Flask(__name__)
 
@@ -59,4 +60,5 @@ def page_not_found(e):
     return render_template('404.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
