62fd6376777e49c0a48340b35961ff59237aaed1
XlentAcademy <xlentclass@gmail.com>
2021-03-23 22:47:32 +0530

    resolved bugs

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def page_not_found(e):
     return render_template('404.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    port = int(os.environ.get('PORT', 33507))
+    app.run(host="0.0.0.0", debug=True, port=port)
