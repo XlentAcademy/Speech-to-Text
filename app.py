cfb3ffc61fa249186a4ecd58c1157160dc58c5e9
XlentAcademy <xlentclass@gmail.com>
2021-03-23 22:51:19 +0530

    resolved bugs

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def page_not_found(e):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 33507))
-    app.run(host="0.0.0.0", debug=True, port=port)
+    app.run(host="127.0.1.0", debug=True, port=port)
